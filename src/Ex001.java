5a0c6652a9efac12eb2e8bf6b669254db578c623
Zawut <sirof@proton.me>
2025-11-20 23:47:24 +0100

    ejercicio 1 creacion

--- src/Ex001.java
+++ src/Ex001.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 
 public class Ex001 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Hello World!");
 	}
 
 }
